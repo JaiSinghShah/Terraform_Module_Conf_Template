bad9f2275fe65d72b1ec6de5a2c663b508edef32
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-11 22:12:57 +0530

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -7,3 +7,7 @@ resource "aws_instance" "app_instance" {
   instance_type = var.instance_type
   subnet_id     = module.networking.subnet_id
 }
+
+output "instance_id" {
+  value = aws_instance.app_instance.id
+}
