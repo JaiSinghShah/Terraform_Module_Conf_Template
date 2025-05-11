9e63ce3e1bb3048e7bf7590118a05fd8fb8a8705
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-11 21:56:42 +0530

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,3 +1,19 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  region = "ap-south-1"  # Specify the region here
+  tags = {
+    Name = "Main_VPC"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"  # Use the availability zone in the ap-south-1 region
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Subnet"
+  }
 }
