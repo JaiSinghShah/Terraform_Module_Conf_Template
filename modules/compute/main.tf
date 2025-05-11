c8f6bb8abcc2ddb6117e4cba97122ea6bf548e0d
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-11 22:08:03 +0530

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,9 +1,9 @@
+module "networking" {
+  source = "../networking"
+}
+
 resource "aws_instance" "app_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = module.networking.subnet_id
-  region        = "ap-south-1"  # Specify the region
-  tags = {
-    Name = "App Instance"
-  }
 }
