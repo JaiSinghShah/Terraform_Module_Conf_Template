047df3af2246c7af55a055d00b09d6d8e4253daa
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-11 21:48:27 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,23 @@
-vpc_cidr      = "10.0.0.0/16"
-ami_id        = "ami-0e35ddab05955cf57"
-instance_type = "t2.micro"
-bucket_name   = "Terraform_Module_Conf_Template"
-region        = "ap-south-1"
-aws_profile   = "default"
+output "vpc_cidr" {
+  value = "10.0.0.0/16"
+}
+
+output "ami_id" {
+  value = "ami-0e35ddab05955cf57"
+}
+
+output "instance_type" {
+  value = "t2.micro"
+}
+
+output "bucket_name" {
+  value = "Terraform_Module_Conf_Template"
+}
+
+output "region" {
+  value = "ap-south-1"
+}
+
+output "aws_profile" {
+  value = "default"
+}
