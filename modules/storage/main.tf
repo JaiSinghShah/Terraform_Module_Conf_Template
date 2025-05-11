59890283887ff54a559fbc1662fc3328dc6a3ed4
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-11 22:42:17 +0530

    Add files via upload

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,6 +1,11 @@
-
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
+  bucket        = var.bucket_name
+  force_destroy = var.force_destroy
+
+  tags = var.tags
 }
-    
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = var.acl
+}
--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,6 +1,22 @@
-
 variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  default     = "Terraform_Module_Conf_Template"
+  description = "The name of the S3 bucket"
+  type        = string
 }
-    
+
+variable "force_destroy" {
+  description = "Whether to delete all objects from the bucket on destroy"
+  type        = bool
+  default     = false
+}
+
+variable "acl" {
+  description = "The canned ACL to apply to the bucket"
+  type        = string
+  default     = "private"
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the bucket"
+  type        = map(string)
+  default     = {}
+}
