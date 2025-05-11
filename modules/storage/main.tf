c59fd2432aede6d16129baf3a44edd4e5f0aa6a8
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-11 21:32:02 +0530

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,4 +1,13 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+  bucket = "terraform-module-conf-template"  # ✅ compliant name
+
+  tags = {
+    Name        = "TerraformModuleConfTemplate"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
   acl    = "private"
 }
