716ca0a78751f995ed849c1ce0d44077dcf24215
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-11 22:00:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-variable "region" {
-  description = "The AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-variable "aws_profile" {
-  description = "AWS CLI profile"
-  type        = string
-  default     = "default"
-}
-
-variable "bucket_name" {
-  description = "S3 bucket name"
-  type        = string
-  default     = "Terraform_Module_Conf_Template"
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
@@ -23,13 +5,19 @@ variable "vpc_cidr" {
 }
 
 variable "ami_id" {
-  description = "AMI ID for EC2 instances"
+  description = "AMI ID to launch the EC2 instance"
   type        = string
   default     = "ami-0e35ddab05955cf57"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "Instance type for the EC2 instance"
   type        = string
   default     = "t2.micro"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "Terraform_Module_Conf_Template"
 }
