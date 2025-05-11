206b4c44f2f438bdf656215e68c9d0cc3d2dabf7
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-11 21:59:34 +0530

    Update variables.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,9 +1,11 @@
 variable "ami_id" {
-  description = "AMI ID for EC2 instance"
+  description = "AMI ID to launch the EC2 instance"
   type        = string
+  default     = "ami-0e35ddab05955cf57"  # Make sure this AMI is available in ap-south-1 region
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "Instance type for the EC2 instance"
   type        = string
+  default     = "t2.micro"
 }
