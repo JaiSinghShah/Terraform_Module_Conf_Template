0898a995f6e1198b8ccccd9d275f2931129d9515
JaiSinghShah <113639120+JaiSinghShah@users.noreply.github.com>
2025-05-11 21:48:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,35 @@
 variable "region" {
-  description = "AWS region to deploy the resources"
+  description = "The AWS region"
   type        = string
-  default     = ap-south-1"  # Or set via CLI/Jenkins input
+  default     = "ap-south-1"
 }
 
 variable "aws_profile" {
-  description = "AWS CLI profile to use"
+  description = "AWS CLI profile"
   type        = string
-  default     = "default"  # Optional, use only if you use AWS profiles
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "AMI ID to use for the EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "Instance type for EC2"
-  type        = string
+  default     = "default"
 }
 
 variable "bucket_name" {
   description = "S3 bucket name"
   type        = string
+  default     = "Terraform_Module_Conf_Template"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "ami-0e35ddab05955cf57"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
